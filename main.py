66a99bf6d6c598f29f32507870d568691147a86f
NeoZMeng <69818758+NeoZMeng@users.noreply.github.com>
2021-12-30 14:07:58 -0800

    improved usability

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-
-
-
 from PlaysAnalysis import PlaysAnalysis
 from PlayerAnalysis import PlayerAnalysis
 from DownsizedData import DownsizedData
 from DataForNN import DataForNN
 
-def initialize():
-    PlaysAnalysis.initialize()
-    PlayerAnalysis.initialize()
-    DownsizedData.initialize()
+def initialize(fileNames):
+    PlaysAnalysis.initialize(fileNames)
+    PlayerAnalysis.initialize(fileNames)
+    DownsizedData.initialize(fileNames)
+    DataForNN.initialize(fileNames)
+    
+    
+    
+#####################################################
+
+# put the corresponding raw data file names below
+
+fileNames = {'plays': 'rawData/plays.csv',
+             'players': 'rawData/players.csv',
+             'games': 'rawData/games.csv',
+             'scouting': 'rawData/PFFScoutingData.csv',
+             'tracking': ['rawData/tracking2018.csv',
+                          'rawData/tracking2019.csv',
+                          'rawData/tracking2020.csv']
+             }
 
 
 
@@ -20,7 +33,7 @@ def initialize():
 
 if __name__ == '__main__':
     # run initialize() when it's brand new
-    initialize()
+    initialize(fileNames)
     DataForNN.prepareData()
 
 
